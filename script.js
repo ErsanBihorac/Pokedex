02bf095523e918c5e086bd8e019222ecf60414dd
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2023-11-30 16:00:37 +0100

    responsiveness fixed, new template function, new pokemons

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let currentPokemon;
-let pokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata', 'raticate', 'spearow', 'fearow', 'ekans', 'arbok', 'pikachu', 'raichu', 'sandshrew', 'sandslash', 'mewtwo'];
+let pokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie',
+'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata', 'raticate', 'spearow',
+'fearow', 'ekans', 'arbok', 'pikachu', 'raichu', 'sandshrew', 'sandslash', 'nidorina', 'nidoqueen',
+'nidorino', 'nidoking', 'clefairy', 'clefable', 'vulpix', 'ninetales', 'jigglypuff', 'wigglytuff', 'zubat', 'golbat', 'oddish',
+'gloom', 'vileplume', 'paras', 'parasect', 'venonat', 'venomoth', 'diglett', 'dugtrio', 'meowth', 'persian', 'psyduck', 'golduck',
+'mankey', 'primeape', 'growlithe', 'arcanine', 'poliwag', 'poliwhirl', 'poliwrath', 'abra', 'kadabra', 'alakazam', 'machop',
+'machoke', 'machamp', 'bellsprout', 'weepinbell', 'victreebel', 'tentacool', 'tentacruel', 'geodude', 'graveler', 'golem',
+'ponyta', 'rapidash', 'slowpoke', 'slowbro', 'magnemite', 'magneton', 'doduo', 'dodrio', 'seel', 'dewgong',
+'grimer', 'muk', 'shellder', 'cloyster', 'gastly', 'haunter', 'gengar', 'onix', 'drowzee', 'hypno', 'krabby', 'kingler',
+'voltorb', 'electrode', 'exeggcute', 'exeggutor', 'cubone', 'marowak', 'hitmonlee', 'hitmonchan', 'lickitung', 'koffing',
+'weezing', 'rhyhorn', 'rhydon', 'chansey', 'tangela', 'kangaskhan', 'horsea', 'seadra', 'goldeen', 'seaking', 'staryu',
+'starmie', 'scyther', 'jynx', 'electabuzz', 'magmar', 'pinsir', 'tauros', 'magikarp', 'gyarados', 'lapras',
+'ditto', 'eevee', 'vaporeon', 'jolteon', 'flareon', 'porygon', 'omanyte', 'omastar', 'kabuto', 'kabutops', 'aerodactyl',
+'snorlax', 'articuno', 'zapdos', 'moltres', 'dratini', 'dragonair', 'dragonite', 'mewtwo'];
 let filtered_pokemon = [];
 let pokemon_url = [];
 
@@ -21,18 +34,27 @@ async function loadPokemon() {
         let response = await fetch(url);
         currentPokemon = await response.json();
 
-        pokedex.innerHTML += /*html*/`
-            <div onclick="openPokemon(${i})" class="pokemon ${currentPokemon['types'][0]['type']['name']}">
-                <div class="pokemon-data">
-                    <div class="pokemon-name">${currentPokemon['name']}</div>
-                    <div class="pokemon-type">${currentPokemon['types'][0]['type']['name']}</div>
-                    <div>#${currentPokemon['order']}</div>
-                </div>
-
-                <img class="img-pokemon" src="${currentPokemon['sprites']['front_default']}">
-            </div>
-        `
+        pokedex.innerHTML += pokemonTemplate(i);
     }
+}
+
+/**
+ * this function is a template function and returns the pokemon card
+ * @param {number} i - pokemon
+ * @returns - a template
+ */
+function pokemonTemplate(i) {
+    return /*html*/`
+    <div onclick="openPokemon(${i})" class="pokemon ${currentPokemon['types'][0]['type']['name']}">
+        <div class="pokemon-data">
+            <div class="pokemon-name">${currentPokemon['name']}</div>
+            <div class="pokemon-type">${currentPokemon['types'][0]['type']['name']}</div>
+            <div>#${currentPokemon['order']}</div>
+        </div>
+
+        <img class="img-pokemon" src="${currentPokemon['sprites']['front_default']}">
+    </div>
+`;
 }
 
 /**
